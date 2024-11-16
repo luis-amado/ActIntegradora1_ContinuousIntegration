5f07b8621fbd04f71ae75f8de6332c2073d737bd
Luis <lamadoalvarez@gmail.com>
2024-11-15 22:37:33 -0600

    Third test

--- tests/tests.cc
+++ tests/tests.cc
@@ -1,11 +1,28 @@
 #include <catch2/catch_test_macros.hpp>
 
 #include "parte1_1.h"
+#include "parte1_2.h"
 #include "parte3.h"
 
 TEST_CASE("zAlg1")
 {
     vector<int> zAlgResult = ZAlg1("test", "fulltestfull");
+    int len = 4;
+    int foundPos = -1;
+    for (int i = 0; i < zAlgResult.size(); i++)
+    {
+        if (zAlgResult[i] == len)
+        {
+            foundPos = i - len - 1;
+            break;
+        }
+    }
+    REQUIRE(foundPos == 4);
+}
+
+TEST_CASE("zAlg2")
+{
+    vector<int> zAlgResult = ZAlg("test", "fulltestfull");
     int len = 4;
     int foundPos = -1;
     for (int i = 0; i < zAlgResult.size(); i++)
